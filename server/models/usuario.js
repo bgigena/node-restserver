222190ad778492b257c9ac29268267f6db056850
Cleo <braian.indu06@gmail.com>
2020-03-05 17:44:25 -0300

    added write on db

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -5,21 +5,22 @@ let Schema = mongoose.Schema;
 let usuarioSchema = new Schema({
     nombre: {
         type: String,
-        require: [true, 'El nombre es necesario']
+        required: [true, 'El nombre es necesario']
     },
     email: {
         type: String,
-        require: [true, 'El correo es necesario']
+        required: [true, 'El correo es necesario']
     },
     password: {
         type: String,
-        require: [true, 'La contraseña es obligatoria']
+        required: [true, 'La contraseña es obligatoria']
     },
     img: {
         type: String,
-        require: false
+        required: false
     },
     role: {
+        type: String,
         default: 'USER_ROLE'
     },
     estado: {
